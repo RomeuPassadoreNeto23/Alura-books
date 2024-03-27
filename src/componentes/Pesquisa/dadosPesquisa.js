687a043d1b3bc7f324a1a8e9293dadb98b461282
Romeu <passadore.neto@outlook.com>
2024-03-27 17:32:36 -0300

    Atualizando estados com onBlur

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,13 @@
+import livro from '../../imagens/livro.png'
+import livro2 from '../../imagens/Star Wars Luz dos Jedi.webp'
+import livro3 from '../../imagens/Star Wars A Velha República  Revan.webp'
+import livro4 from '../../imagens/Star Wars Horizonte da Meia noite  The High Republic.webp'
+
+
+
+export const Livros = [
+    { nome: 'Liderança em Design', id: 1, src: livro },
+    { nome: 'Star Wars: Luz dos Jedi', id: 2, src: livro2 },
+    { nome: 'Star Wars: A Velha República - Revan ', id: 3, src: livro3 },
+    { nome: 'Star Wars: Horizonte da Meia-noite - The High Republic', id: 4, src: livro4 },
+]
--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,9 @@
 import Input from "../Input"
 import styled from "styled-components"
+import { useState } from "react"
+import { Livros } from './dadosPesquisa'
 
-   const PesquisaContainer = styled.section`
+const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
         color: #FFF;
         text-align: center;
@@ -9,29 +11,39 @@ import styled from "styled-components"
         height: 270px;
         width: 100%;
    `
-   const Titulo = styled.h2`
+const Titulo = styled.h2`
         color: #FFF;
         font-size: 36px;
         text-align: center;
         width: 100%;
    `
-   const Subtitulo = styled.h3`
+const Subtitulo = styled.h3`
         font-size: 16px;
         font-weight: 500;
         margin-bottom: 40px;
    `
 
 function Pesquisa() {
-
+    const [LivrosPesquisados, setLivrosPesquisados] = useState([])
+    console.log(LivrosPesquisados)
     return (
 
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar!</Titulo>
             <Subtitulo>Encontre Seu Livor em na nossa estante</Subtitulo>
-            <Input placeholder="Escreva sua próxima Leitura" />
+            <Input placeholder="Escreva sua próxima Leitura"
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = Livros.filter(livor => livor.nome.includes(textoDigitado))
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
+            />
+            
         </PesquisaContainer>
+
 
     )
 
 }
+
 export default Pesquisa
