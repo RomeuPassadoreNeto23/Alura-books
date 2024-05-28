d1199732874d14595f775f9bd0ac4d1181f7fa34
Romeu <passadore.neto@outlook.com>
2024-05-28 17:15:39 -0300

    POST e DELETE de favoritos

--- src/servicos/favoritos.js
+++ src/servicos/favoritos.js
@@ -2,13 +2,27 @@ import axios from "axios";
 
 const favoritosAPI = axios.create({ baseURL: "http://localhost:8000/favoritos" })
 
- async function getfavoritos() {
+async function getfavoritos() {
+
     const response = await favoritosAPI.get('/')
 
     return response.data
 }
+async function postFavoritos(id) {
+
+    await favoritosAPI.post(`${id}`)
+
+}
+async function deleteFavoritos(id) {
+
+    await favoritosAPI.delete(`${id}`)
+
+}
+
 export {
 
-    getfavoritos
+    getfavoritos,
+    deleteFavoritos,
+    postFavoritos
 
 }
